2b161462f3c173308784ff88d79ce4f5f1440adb
shahnmah <shahn.mah@gmail.com>
2022-05-07 22:37:30 +0600

    uri cunnected

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors())
+app.use(express.json())
+
+
+// lVXdyWmDP8IRJuoH  user
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.1psxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('okay')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
