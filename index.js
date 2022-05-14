3ecc06cf65b54d27eeb07b4eeec6fa0341c20a30
shahnmah <shahn.mah@gmail.com>
2022-05-15 00:16:14 +0600

    node start script added

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server for inventory website')
 })
 
 app.listen(port, () => {
