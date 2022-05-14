14819e057182d115f87339ffa72c152c6cbfff70
shahnmah <shahn.mah@gmail.com>
2022-05-14 23:23:28 +0600

    all api added

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
-// lVXdyWmDP8IRJuoH  user
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.1psxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
